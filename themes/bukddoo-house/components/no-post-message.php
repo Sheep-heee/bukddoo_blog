610c6a9866ed9a336d8d67b4df739e82beb627a8
Sheep-heee <sheephi0609@gmail.com>
2025-06-26 23:48:55 +0900

    no-post refactoring

--- themes/bukddoo-house/components/no-post-message.php
+++ themes/bukddoo-house/components/no-post-message.php
@@ -1,26 +1,16 @@
 <?php
   $context = $args['context'] ?? 'default';
+  $is_home = ($context === 'home');
+
+  $wrapper_tag = $is_home ? 'article' : 'li';
+  $wrapper_class = $is_home ? 'main-post-card is-empty' : 'no-post-default is-empty';
 ?>
 
-<?php
-  if ($context === 'home') {
-    echo '<article class="main-post-card is-empty">';
-  } else {
-    echo '<li class="no-post-default is-empty">';
-  }
-?>
-
+<<?= $wrapper_tag ?> class="<?= esc_attr($wrapper_class) ?>">
 <div class="no-post">
   <div class="no-post-icon">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/common/no-post.svg" alt="글이 없어서 드러누운 뚜뚜" />
   </div>
   <div class="no-post-text">글이 없어요.</div>
 </div>
-
-<?php
-  if ($context === 'home') {
-    echo '</article>';
-  } else {
-    echo '</li>';
-  }
-?>
+</<?= $wrapper_tag ?>>
